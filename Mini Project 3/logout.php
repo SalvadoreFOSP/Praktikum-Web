d14d0c988e18f7492bed6993441261a8f936822f
SalvadoreFOSP <126777568+SalvadoreFOSP@users.noreply.github.com>
2024-04-14 21:46:40 +0800

    Delete Mini Project 3 directory

--- Mini Project 3/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Menghapus semua variabel sesi
-session_destroy(); // Menghapus sesi
-
-// Arahkan kembali ke halaman login atau halaman lain yang sesuai
-header("Location: index.php");
-exit;
-?>
--- Mini Project 3/script.js
+++
@@ -1,9 +0,0 @@
-function calculateTotal() {
-    var startDate = new Date(document.getElementById('start_date').value);
-    var endDate = new Date(document.getElementById('end_date').value);
-    var timeDiff = endDate.getTime() - startDate.getTime();
-    var daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24)); // Perbedaan hari
-    var price = document.getElementById('price').value;
-    var totalPrice = daysDiff * price; // Harga total
-    document.getElementById('total_price').value = totalPrice;
-}
